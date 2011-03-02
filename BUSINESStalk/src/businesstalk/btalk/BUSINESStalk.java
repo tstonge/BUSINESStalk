7486739b9f9ae3872591efff2f317e252f221b1f
Travis St. Onge <MacBook@macbook.olin.edu>
2011-03-01 22:22:55 -0500

    added buttons and camera intent

--- BUSINESStalk/src/businesstalk/btalk/BUSINESStalk.java
+++ BUSINESStalk/src/businesstalk/btalk/BUSINESStalk.java
@@ -1,7 +1,11 @@
 package businesstalk.btalk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class BUSINESStalk extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,14 @@ public class BUSINESStalk extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button takephoto = (Button)findViewById(R.id.takephoto);
+        
+        takephoto.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Intent camera = new Intent(BUSINESStalk.this, Camera.class);
+        		startActivity(camera);
+        	}
+        });
     }
 }
