56df6e7f86efc1c8317a3e21fbcae2846f4abbac
travisstonge <stonge@students.olin.edu>
2011-03-03 12:57:37 -0500

    added schtuff

--- BUSINESStalk/src/businesstalk/btalk/BUSINESStalk.java
+++ BUSINESStalk/src/businesstalk/btalk/BUSINESStalk.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -11,7 +12,9 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class BUSINESStalk extends Activity {
-    /** Called when the activity is first created. */
+    private static final int PICTURE_RESULT = 0;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,15 @@ public class BUSINESStalk extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
         		WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+        
+        Button idCustomer = (Button)findViewById(R.id.idCustomer);
+        
+        idCustomer.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		doTakePhotoAction();
+        	}
+        });
+        
         
         Button newID = (Button)findViewById(R.id.newID);
         
@@ -28,6 +40,11 @@ public class BUSINESStalk extends Activity {
         		startActivity(newid);
         	}
         });
+    }
+    
+    private void doTakePhotoAction() {
+    	Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);   
+        this.startActivityForResult(camera, PICTURE_RESULT);
     }
     
     @Override
